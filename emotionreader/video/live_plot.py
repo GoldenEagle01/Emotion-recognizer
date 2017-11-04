026405206210735baedc9885ab4a4b10906a1445
andrei.marin@iris-rbotics.com <marinlud@gmail.com>
2017-11-04 04:23:51 +0200

    Plot emoji

--- emotionreader/video/live_plot.py
+++ emotionreader/video/live_plot.py
@@ -1,38 +1,94 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
 import plotly.plotly as py
 import time
+import datetime
+import warnings
+import matplotlib.cbook
+import matplotlib.image as mpimg
+import matplotlib.animation as animation
+from matplotlib import style
+
+time_display = time.time()
 
 def init():
-    plt.get_backend()
     plt.show()
-    print "starting plot"
+    print "Starting plot window"
+    time_display = time.time()
 
-def plot(array):
+def plot(array, prediction):
+    warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+    matplotlib.use("TkAgg")
     #normalize array
-    array_percent = [ round(x * 100, 0) for x in array]
+    array_percent = [ int(round(x * 100, 0)) for x in array]
 
     N = len(array)
     emotions = range(N)
 
-    print array_percent
+#if len(array_percent) is not 0:
+    #print array_percent
     #print emotions
 
     colors = ['red', 'purple', 'green', 'black', 'blue', 'yellow']
 
+    # plot window 1 - graph 1
+    #plt.figure(1)
+    plt.rcParams["figure.figsize"] = [7.9, 9]
+    plt.subplot(221)
     plt.bar(emotions, array_percent, color=colors, width=0.9)
     plt.ylim([0,100])
     plt.title("Live Feed Plot")
     plt.xlabel("   Anger       Disgust       Happy       Neutral      Sadness      Surprise")
     plt.ylabel("Percentage")
+
+    # plot window 1 - graph 1
+    plt.subplot(222)
+    if prediction == 0:
+        image = mpimg.imread("emoji/anger.png")
+        plt.imshow(image)
+    elif prediction == 1:
+        image = mpimg.imread("emoji/disgust.png")
+        plt.imshow(image)
+    elif prediction == 2:
+        image = mpimg.imread("emoji/happy.png")
+        plt.imshow(image)
+    elif prediction == 3:
+        image = mpimg.imread("emoji/neutral.png")
+        plt.imshow(image)
+    elif prediction == 4:
+        image = mpimg.imread("emoji/sad.png")
+        plt.imshow(image)
+    elif prediction == 5:
+        image = mpimg.imread("emoji/surprise.png")
+        plt.imshow(image)
+
+#plt.subplot(224)
+#plt.cla()
+
+#plt.figure(2)
+    plt.subplot(223)
+    plt.ylim([0,100])
+    time_2 = round(time.time() - time_display, 2)
+
+    print time_2
+    if len(array_percent) is not 0:
+        print array_percent[prediction]
     
-    plt.pause(.001)
-    plt.clf()      
+    # plot
+    if len(array_percent) is not 0:
+        plt.scatter(time_2, array_percent[prediction])
+    plt.gcf().autofmt_xdate()
+    
+    # update plot
+    plt.pause(.1)
+    #plt.clf()
 
 if __name__== "__main__":
-    
+    matplotlib.use("TkAgg")
     for x in range(0, 1000):
         plot()
         time.sleep(0.1)
         
-
--- emotionreader/video/webcam.py
+++ emotionreader/video/webcam.py
@@ -2,10 +2,13 @@ import pickle
 import cv2
 import numpy as np
 from frames import FrameHandler
+import matplotlib
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import plotly.plotly as py
 from live_plot import plot
+from live_plot import init
 
 def record(filename, seconds, **kwargs):
    
@@ -13,7 +16,7 @@ def record(filename, seconds, **kwargs):
     
     fourcc = kwargs.get('fourcc', ('X', 'V', 'I', 'D'))
     frame_size = kwargs.get('size', (640, 480))
-    fps = kwargs.get('fps', 25)
+    fps = kwargs.get('fps', 20)
     codec = cv2.cv.CV_FOURCC(*fourcc)
     
     cap.set(cv2.cv.CV_CAP_PROP_FPS, fps)
@@ -47,6 +50,9 @@ def get_webcam_video(width, height):
 
 
 def predict_from_webcam(args):
+    #init plot
+    init()
+    
     emotions = ['anger', 'disgust','happy', 'neutral', 'sadness', 'surprise']
     
     with open('models/trained_svm_model') as f:
@@ -55,7 +61,9 @@ def predict_from_webcam(args):
     width, height = args.dimensions
     for frame in get_webcam_video(width, height):
         handler = FrameHandler(frame)
-        
+        array_data = np.array([])
+        prediction_data = 0
+
         if args.landmarks:
             handler.draw_landmarks()
         
@@ -64,14 +72,21 @@ def predict_from_webcam(args):
             prediction = model.predict(faces)
             if len(prediction) > 0:
                 text = emotions[prediction[0]]
-                cv2.putText(handler.frame, text, (40, 40),
+                prediction_data = prediction[0]
+                cv2.putText(handler.frame, text, (60, 60),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),
                             thickness=2)
                 array = model.predict_proba(faces)
-                plot (array[0])
+                array_data = np.array(array[0])
+        else:
+            prediction_data = 6
+
+        #plot data
+        if array_data is not None:
+            plot(array_data, prediction_data)
 
         cv2.imshow('image', handler.frame)
-        cv2.moveWindow('image', 750,45);
+        cv2.moveWindow('image', 800, 30);
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
